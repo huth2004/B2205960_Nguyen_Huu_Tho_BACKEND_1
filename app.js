ea079e48cb9f59d17acbb985d7948675e491b341
Huth <huutho846oz@gmail.com>
2025-10-12 21:48:05 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const ApiError = require('./app/api-error');
 
 const app = express();
 const contactRoutes = require('./app/routes/contact.route');
@@ -7,6 +8,16 @@ const contactRoutes = require('./app/routes/contact.route');
 app.use(cors());
 app.use(express.json());
 app.use('/api/contacts', contactRoutes);
+
+app.use((req, res, next) => {
+  return next(new ApiError(404, 'Resource not found'));
+});
+
+app.use((err, req, res, next) => {
+  return res.status(err.statusCode || 500).json({
+    message: err.message || 'Internal Server Error',
+  });
+});
 
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to contact book application.' });
